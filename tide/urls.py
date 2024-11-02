b2133c1a8c13ed6499837d60a4a9ca14f90ef1b1
Paul Martin Enano <paul.m.enano@gmail.com>
2024-11-02 18:28:39 -0400

    added functionality for friend model

--- tide/urls.py
+++ tide/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('profile/<int:pk>/friend_suggestions/', views.ShowFriendSuggestionsView.as_view(), name='friend_suggestions'),
     path('profile/<int:pk>/add_friend/<int:other_pk>/', views.CreateFriendView.as_view(), name='add_friend'),
 
+
     
 
 ]
--- tide/views.py
+++ tide/views.py
@@ -1,21 +1,16 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import View, ListView, DetailView, CreateView
 from .models import Profile
 from .forms import CreateProfileForm
 from django.urls import reverse_lazy
-
-# Display all profiles
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from .models import Profile
-
-# View to show all profiles
-# views.py
 from django.shortcuts import redirect
+
+from tide import models
 
 class ShowAllProfilesView(ListView):
     model = Profile
@@ -24,15 +19,13 @@ class ShowAllProfilesView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Check if the logged-in user has a profile (optional if you need it)
         if self.request.user.is_authenticated:
             context['has_profile'] = Profile.objects.filter(user=self.request.user).exists()
         return context
     
-# View to show a single profile
 class ShowProfilePageView(LoginRequiredMixin, DetailView):
     model = Profile
-    template_name = 'tide/show_profile.html'  # Update to match your app name
+    template_name = 'tide/show_profile.html'  
     context_object_name = 'profile'
     login_url = '/login/'
 
@@ -57,11 +50,9 @@ class CreateProfileView(CreateView):
     template_name = 'tide/create_profile.html'
 
     def form_valid(self, form):
-        # Save the form to create the user and profile
         self.object = form.save()
         
-        # Redirect to the profile page of the created user
-        return redirect('show_profile', pk=self.object.pk)  # Redirect to the specific profile's detail view
+        return redirect('show_profile', pk=self.object.pk) 
 
 # views.py
 from django.urls import reverse
